f334e4de8339e32a0c906b81c06f66757ffe16d4
Tyler Torola <Tyler@TJT.Codes>
2019-02-03 13:53:22 -0500

    Stamp 1.2

---
+++ 1.2.js
@@ -0,0 +1,56 @@
+const rand = limit => Math.floor(Math.random() * limit);
+const sample = arr => arr[rand(arr.length)];
+const chance = ratio => Math.random() <= ratio;
+const mod = (x, y) => ((x % y) + y) % y
+
+const WIDTH = 800;
+const HEIGHT = 500;
+
+const DIRS = ['UP', 'DOWN', 'LEFT', 'RIGHT'];
+const DIR_DELTAS = {
+  UP: {
+    y: -1,
+    x:  0
+  },
+  DOWN: {
+    y:  1,
+    x:  0,
+  },
+  LEFT: {
+    y:  0,
+    x: -1
+  },
+  RIGHT: {
+    y:  0,
+    x:  1
+  }
+}
+
+const makePt = () => ({
+  x: rand(WIDTH),
+  y: rand(HEIGHT),
+  dir: sample(DIRS)
+});
+
+const progressPt = pt => ({
+  x: mod(pt.x + DIR_DELTAS[pt.dir].x, WIDTH),
+  y: mod(pt.y + DIR_DELTAS[pt.dir].y, HEIGHT),
+  dir: chance(.99) ? pt.dir : sample(DIRS)
+});
+
+const drawPt = pt => {
+  point(pt.x, pt.y);
+};
+
+let pts = [...new Array(1000)].map(makePt);
+
+function setup() {
+  createCanvas(WIDTH, HEIGHT);
+}
+
+function draw() {
+  fill('rgba(255, 255, 255, .1)');
+  rect(-1, -1, WIDTH + 1, HEIGHT + 1);
+  pts.forEach(drawPt);
+  pts = pts.map(progressPt);
+}
